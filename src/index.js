eaf67e1630582dc74acf7dffdabc6a02521c0f8f
RKrueg <thegckid@gmail.com>
2023-01-10 19:46:38 -0800

    clean up our index.js with correct indentation and deletion of our commented out code

--- src/index.js
+++ src/index.js
@@ -13,51 +13,24 @@ function getMeal(meal) {
   });
 }
 
-// let text = data.results.map(meal => `${meals.category}<br>
-// ${meals.strMeal}<br>
-// ${meals.strArea}<br>
-// ${meals.strInstructions}<br>
-// ${question.question}<br><br>`).join('');
-
-// document.getElementById("demo").innerHTML = text;
-
-// let text = "";
-// for(let i = 0; i < data.results.length; i++){
-
-//     text += data.results[i].category + "<br>"
-//     + data.results[i].correct_answer + "<br>"
-//     + data.results[i].difficulty + "<br>"
-//     + data.results[i].incorrect_answers + "<br>"
-//     + data.results[i].question + "<br><br><br>"
-// }
-
-
-// document.getElementById("demo").innerHTML = text;
-
-
-
-
 function printElements(data) {
   console.log(data[0])
   let text = "";
-  // let mealChoices = document.getElementById('showImage');
+  //const dishIngredients = [];
   for (let i = 0; i < data[0].meals.length; i++) {
-    text += `<div class="showImg"> <img src="${data[0].meals[i].strMealThumb}"/></div> \n \n Your perfect meal focused around ${data[1]} would be ${data[0].meals[i].strMeal}.\n \n Step by step instructions to make your significant other love the food you cooked:\n \n ${data[0].meals[i].strInstructions} \n \n`;
-    // mealChoices.innerHTML = `
-    // <div class="showImg">
-    //   <img src="${data[0].meals[i].strMealThumb}" alt="${data[0].meals[i].strMeal} image"/>
-    //   </div>
-    //   <p class="showResponse">${text}</p>`;
-  }
-  document.querySelector('#showResponse').innerHTML = text;
-
-  // let html = <div class = "showImage"> 
-  // <img src = "${data[0].meal[i].strMealThumb}" alt = ""></img>
-  // </div>
-  // ;
-  // document.querySelector('showImage').innerHTML = html;
+   // if (data[0].meals[`strIngredient${i}`]) {
+   //   dishIngredients.push(`${data[0].meals[`strIngredient${i}`]}`)
+    
+   // console.log(data[0].meals[`strIngredient${i}`])
+    text += `<div class="dish"> <h3 class="dishName"> ${data[0].meals[i].strMeal}</h3></div>
+    <div class="showArea"><h5>${data[0].meals[i].strArea} meal</h5></div>
+    <div class="showImg"> <img src="${data[0].meals[i].strMealThumb}"/></div>
+    <div class="showInstructions"><h5>Directions</h5></div>
+    <a href=${data[0].meals[i].strYoutube}>Check Out How To Video Here!</a>
+    <p class="instructions">${data[0].meals[i].strInstructions}</p>`;
 }
-
+  document.querySelector('#showResponse').innerHTML = text;
+}
 
 function printError(error) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the meal data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
